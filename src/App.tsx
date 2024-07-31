4dbf128d9faa19042675e0f4d65bf53f78d771ea
Rakesh Lakshminarayana <rakeshl84@outlook.com>
2024-07-31 13:57:00 +1000

    add syncfusion controls

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
 import ProjectDrawing from './pages/project-drawing';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <ProjectDrawing />
--- src/pages/project-drawing.tsx
+++ src/pages/project-drawing.tsx
@@ -1,5 +1,47 @@
+import {
+  DiagramComponent,
+  HierarchicalTree,
+  MindMap,
+  RadialTree,
+  ComplexHierarchicalTree,
+  DataBinding,
+  Snapping,
+  PrintAndExport,
+  BpmnDiagrams,
+  SymmetricLayout,
+  ConnectorBridging,
+  UndoRedo,
+  LayoutAnimation,
+  DiagramContextMenu,
+  ConnectorEditing,
+  Ej1Serialization,
+  Inject,
+} from '@syncfusion/ej2-react-diagrams';
+
 const ProjectDrawing = () => {
-  return <div>project-drawing</div>;
+  return (
+    <DiagramComponent id="diagram" width={1000} height={500}>
+      <Inject
+        services={[
+          HierarchicalTree,
+          MindMap,
+          RadialTree,
+          ComplexHierarchicalTree,
+          DataBinding,
+          Snapping,
+          PrintAndExport,
+          BpmnDiagrams,
+          SymmetricLayout,
+          ConnectorBridging,
+          UndoRedo,
+          LayoutAnimation,
+          DiagramContextMenu,
+          ConnectorEditing,
+          Ej1Serialization,
+        ]}
+      />
+    </DiagramComponent>
+  );
 };
 
 export default ProjectDrawing;
